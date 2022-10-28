ecfcc43d747b53fb5161b21e97f22a22bf6571c7
Joseph Thomas P <mail2josephthomas@gmail.com>
2022-10-28 02:28:20 +0000

    ebs-lambda.py edited online with Bitbucket

--- AWS/ebs-lambda.py
+++ AWS/ebs-lambda.py
@@ -1,19 +1,26 @@
 import boto3
-ec2 = boto3.resource('ec2',region_name='eu-west-3')
+
 def lambda_handler(event, context):
+    ec2 = boto3.resource('ec2',region_name='us-east-1')
+    
     for vol in ec2.volumes.all():
+        
         if  vol.state=='available':
             if vol.tags is None:
                 vid=vol.id
-                v=ec2.Volume(vol.id)
-                v.delete()
+                vol.delete()
                 print ('Deleted ' +vid)
                 continue
+            tag_keys = {}
             for tag in vol.tags:
-                if tag['Key'] == 'Name':
-                    value=tag['Value']
-                    if value != 'DND' and vol.state=='available':
+                tag_keys[tag['Key']] = tag['Value']
+                
+            if 'deletePolicy' in tag_keys.keys():
+                    if tag_keys['deletePolicy'] != 'DND':
                         vid=vol.id
-                        v=ec2.Volume(vol.id)
-                        v.delete()
+                        vol.delete()
                         print ('Deleted ' +vid)
+            else:
+                vid=vol.id
+                vol.delete()
+                print ('Deleted ' +vid)
